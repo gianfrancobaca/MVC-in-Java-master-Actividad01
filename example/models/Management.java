9f553c0d0f116724b678e031f16f82f6746a0413
Gianfranco Baca <gianfrancobacapol@gmail.com>
2026-04-16 17:23:33 -0500

    Modelo invitado creado

---
+++ example/models/Management.java
@@ -0,0 +1,13 @@
+package models;
+
+public class Invitado {
+	private String nombre;
+	private String celular;
+	private String genero;
+
+	public Invitado(String nombre, String celular, String genero) {
+		this.nombre = nombre;
+		this.celular = celular;
+		this.genero = genero;
+	}
+}
